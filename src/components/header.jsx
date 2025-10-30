45a755834ead89ecb8ec452d08c05fbb31508f46
Tomas Kimmel <totokimmel@gmail.com>
2025-10-30 20:00:21 -0300

    Finish header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,20 @@
+import Logo from '../assets/LogoLupusTexto.svg'
+
 function Header() {
     return(
     <>
-    
+    <header className="h-20 bg-lupus-light-gray flex items-center justify-between px-8">
+        <div>
+            <img src={Logo} alt="Logo Lupus" className='h-[120px] drop-shadow-md' />
+        </div>
+        <nav>
+            <ul className='flex gap-9 text-lg font-medium text-lupus-primary text-center text-[22px] text-shadow-md'>
+                <li><a href="#" className='whitespace-nowrap hover:text-lupus-primary-dark transition'>Inicio</a></li>
+                <li><a href="#" className='whitespace-nowrap hover:text-lupus-primary-dark transition'>Sobre Nosotros</a></li>
+                <li><a href="#" className='whitespace-nowrap hover:text-lupus-primary-dark transition'>Contactanos</a></li>
+            </ul>
+        </nav>
+    </header>
     </>
     )
 }
